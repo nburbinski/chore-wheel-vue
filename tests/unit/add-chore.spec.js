affc2f6056fea7ad961da8d1a9d682e6ffb9fc22
Nicolas Burbinski <nburbinski@gmail.com>
2022-05-20 22:45:23 -0400

    refactored component

--- tests/unit/add-chore.spec.js
+++ tests/unit/add-chore.spec.js
@@ -14,60 +14,46 @@ describe("AddChore.vue", () => {
     expect(wrapper.exists()).toBe(true);
   });
 
+  it("renders form", () => {
+    const wrapper = shallowMount(AddChore, {
+      global: {
+        plugins: [store],
+      },
+    });
+    expect(wrapper.find("form").exists()).toBe(true);
+    expect(wrapper.findAll("input").length).toBe(2);
+    expect(wrapper.findAll("select").length).toBe(1);
+    expect(wrapper.findAll("option").length).toBe(4);
+  });
 
-  // Refactored into Chore List Component
+  it("doesn't run on empty submit", async () => {
+    const wrapper = shallowMount(AddChore, {
+      global: {
+        plugins: [store],
+      },
+    });
+    await wrapper.find("form").trigger("submit.prevent");
 
-  // it("renders form", () => {
-  //   const wrapper = shallowMount(AddChore, {
-  //     global: {
-  //       plugins: [store],
-  //     },
-  //   });
-  //   expect(wrapper.find("form").exists()).toBe(true);
-  //   expect(wrapper.findAll("input").length).toBe(2);
-  //   expect(wrapper.findAll("select").length).toBe(1);
-  //   expect(wrapper.findAll("option").length).toBe(4);
+    expect(wrapper.emitted("add")).toBe(undefined);
+  });
 
-  //   expect(wrapper.find("button").exists()).toBe(true);
-  // });
+  it("runs on valid submit", async () => {
+    const wrapper = shallowMount(AddChore, {
+      global: {
+        plugins: [store],
+      },
+    });
+    const inputs = wrapper.findAll("input");
 
-  // it("renders label", () => {
-  //   const wrapper = shallowMount(AddChore, {
-  //     global: {
-  //       plugins: [store],
-  //     },
-  //   });
-  //   expect(wrapper.findAll("label").length).toBe(3);
-  // });
+    // Check initial length
+    expect(store.state.chores.length).toBe(3);
 
-  // it("doesn't run on empty submit", async () => {
-  //   const wrapper = shallowMount(AddChore, {
-  //     global: {
-  //       plugins: [store],
-  //     },
-  //   });
-  //   await wrapper.find("form").trigger("submit.prevent");
+    // Set inputs and add chore
+    await inputs.at(0).setValue("test chore");
+    await inputs.at(1).setValue("2019-03-15");
+    await wrapper.find("select").setValue("Nic");
+    await wrapper.find("form").trigger("submit.prevent");
 
-  //   expect(wrapper.emitted("add")).toBe(undefined);
-  // });
-
-  // it("runs on valid submit", async () => {
-  //   const wrapper = shallowMount(AddChore, {
-  //     global: {
-  //       plugins: [store],
-  //     },
-  //   });
-  //   const inputs = wrapper.findAll("input");
-
-  //   // Check initial length
-  //   expect(store.state.chores.length).toBe(2);
-
-  //   // Set inputs and add chore
-  //   await inputs.at(0).setValue("test chore");
-  //   await inputs.at(1).setValue("2019-03-15");
-  //   await wrapper.find("select").setValue("Nic");
-  //   await wrapper.find("form").trigger("submit.prevent");
-
-  //   expect(store.state.chores.length).toBe(3);
-  // });
+    expect(store.state.chores.length).toBe(4);
+  });
 });
