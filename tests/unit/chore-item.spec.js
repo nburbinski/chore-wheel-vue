293d293fefcb15c8f6fe8506afbe5bbfb78368d0
Nicolas Burbinski <nburbinski@gmail.com>
2022-05-20 22:45:04 -0400

    removed unused files

--- tests/unit/chore-item.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import ChoreItem from "@/components/ChoreItem.vue";
-
-describe("ChoreItem.vue", () => {
-  const wrapper = shallowMount(ChoreItem);
-  it("renders Chore component", () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-});
